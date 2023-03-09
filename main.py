78d73043146d364c38d00fdd2d09afb0a0412634
OwenPastos <127431443+OwenPastos@users.noreply.github.com>
2023-03-09 13:00:05 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,58 @@
+def encode(password):
+    new_password = ''
+    for element in str(password):
+        if element == '9':
+            new_password += '2'
+        elif element == '8':
+            new_password += '1'
+        elif element == '7':
+            new_password += '0'
+        else:
+            char = int(element) + 3
+            new_password += str(char)
+
+    return new_password
+
+
+def decode(password):
+    new_password = ''
+    for element in str(password):
+        if element == '2':
+            new_password += '9'
+        elif element == '1':
+            new_password += '8'
+        elif element == '0':
+            new_password += '7'
+        else:
+            char = int(element) - 3
+            new_password += str(char)
+
+    return new_password
+
+
+def print_menu():
+    print('menu')
+    print('-------------')
+    print('1. Encode')
+    print('2. Decode')
+    print('3. Quit\n')
+
+
+
+def main():
+    while True:
+        print_menu()
+        user_input = input('Please enter an option: ')
+        if user_input == '1':
+            password = input('Please enter your password to decode: ')
+            encoded = encode(password)
+            print('Your password has been encoded and stored\n')
+        elif user_input == '2':
+            decoded = decode(encoded)
+            print(f'The encoded password is {encoded}, and the original password is {decoded}.\n')
+        elif user_input == '3':
+            quit()
+
+
+if __name__ == '__main__':
+    main()
